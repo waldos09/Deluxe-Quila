cdb66fc970dfdedea15c7dc8ab98b62575004f4b
waldos09 <legos567@gmail.com>
2023-02-26 22:07:12 -0600

    a0.2

--- pages/Val.js
+++ pages/Val.js
@@ -8,9 +8,9 @@ export default function App() {
         <div background="#F9D423">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-group">
-                <label for="exampleInputEmail1">Fecha de Plantacion</label>
+                <label htmlFor="exampleInputEmail1">Fecha de Plantacion</label>
                 <input type='date' {...register("firstName")} /><br></br>
-                <label for="exampleInputEmail1">Fecha de Recoleccion</label>
+                <label htmlFor="exampleInputEmail1">Fecha de Recoleccion</label>
                 <input type='date' {...register("firstName")} /><br></br>
                 <input type='image' />
                 <input type="submit" />
